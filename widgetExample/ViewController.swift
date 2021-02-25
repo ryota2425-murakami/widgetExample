d55bf7cefc2d9f82fe7847d87f2a561357157ed6
Your Name <you@example.com>
2021-02-26 02:08:08 +0900

    Initial Commit

---
+++ widgetExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  widgetExample
+//
+//  Created by 村上諒太 on 2021/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
